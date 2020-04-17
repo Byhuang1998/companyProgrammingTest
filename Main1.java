27b202a6f327c22a0b556039e39539eede50d801
Byhuang1998 <Byhuang1998@gmail.com>
2020-04-18 00:06:26 +0800

    first - 美团

---
+++ Main1.java
@@ -0,0 +1,62 @@
+package programmingTest;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main1 {
+	static ArrayList<Integer> list = new ArrayList<>();
+	public int getMax(int[] arr) {
+		int len = arr.length;
+		int tem = arr[0];
+		for (int i=1; i<len; ++i) {
+			if (arr[i] > tem) {
+				tem = arr[i];
+			}
+		}
+		return tem;
+		
+	}
+	
+	public void getMaxIndex(int[] arr, int max) {
+		int len = arr.length;
+		for (int i=0; i<len; i++) {
+			if (arr[i] == max) {
+				list.add(i);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main1 t = new Main1();
+		Scanner in = new Scanner(System.in);
+		// n个学生， m个学科
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int[][] arr = new int[n][m];
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<m; j++) {
+				arr[i][j] = in.nextInt();
+			}
+		}
+		in.close();
+		for (int i=0; i<m; ++i) {
+			int[] subjects = new int[n];
+			for (int j=0; j<n; ++j) {
+				subjects[j] = arr[j][i];
+			}
+			t.getMaxIndex(subjects, t.getMax(subjects));
+		}
+		int[] flag = new int[n];
+		for (int num : list) {
+			flag[num] = 1;
+		}
+		int count = 0;
+		for (int k=0; k<n; ++k) {
+			if (flag[k] == 1) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+}
